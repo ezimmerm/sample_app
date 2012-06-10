bb2a3fcd2e711eb3734edfebe1110b284dbcba17
Ethan Zimmermann <ezimmerm@oberlin.edu>
2012-06-09 20:23:42 -0500

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
 
-  get "static_pages/help"
+  match '/signup', to: 'users#new'
+  match '/home', to: 'static_pages#home'
+  match '/help', to: 'static_pages#help'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
-  get "static_pages/about"
-  # The priority is based upon order of creation:
+# The priority is based upon order of creation:
   # first created -> highest priority.
 
   # Sample of regular route:
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,37 +4,53 @@ describe "StaticPages" do
   describe "Home page" do
     it "should have the h1 'Sample App'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit home_path
       page.should have_selector('h1', :text => 'Sample App')
     end
-    it "should have the title 'Home'" do
-      visit '/static_pages/home'
-        page.should have_selector('title',
-                            :text => "Ruby on Rails Tutorial Sample App | Home")
+    it "should have the base title" do
+      visit home_path
+      page.should have_selector('title',
+                            :text => "Ruby on Rails Tutorial Sample App")
                             end
+  it "should not have a custom title" do
+   visit home_path
+   page.should_not have_selector('title', text: '| Home')
   end
+end
 
 describe "Help page" do
   it "should have the h1 'Help'"do
-    visit '/static_pages/help'
+    visit help_path
     page.should have_selector('h1', :text =>'Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
         page.should have_selector('title',
                             :text => "Ruby on Rails Tutorial Sample App | Help")
                             end
     end
 describe "About page" do
 
-    it "should have the h1 'About Us'" do
-          visit '/static_pages/about'
-                page.should have_selector('h1', :text => 'About Us')
+    it "should have the h1 'About'" do
+          visit about_path
+                page.should have_selector('h1', :text => 'About us')
                     end
                     it "should have the title 'About us'" do
-                      visit '/static_pages/about'
+                      visit about_path
                         page.should have_selector('title',
                                             :text => "Ruby on Rails Tutorial Sample App | About us")
                                             end
                       end
-                      end
+
+describe "Contact page" do
+
+  it "should have the h1 'Contact'" do
+    visit contact_path
+    page.should have_selector('h1', :text =>  "Contact")
+  end
+  it "should have the title 'Contact'" do
+    visit contact_path
+    page.should have_selector('title', :text =>  "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+end
